7d9b263b5ecdc3bef47c8f024d8a6bfb2520dbca
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-06-29 07:05:10 -0300

    :bug: fix: id mapping on user creation

--- src/api/createUser.ts
+++ src/api/createUser.ts
@@ -1,10 +1,14 @@
 import { map, Observable } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import { User } from '../components/SelectedUser/SelectedUser';
+import { UserResponse } from './getUsers';
 
-export type CreateUser = Pick<User, "name" | "age">;
+export type CreateUser = Omit<User, "userID">;
 
 export function createUser(user: CreateUser): Observable<User> {
   const url = "http://localhost:3200/users";
-  return ajax.post<User>(url, user).pipe(map((response) => response.response));
+  return ajax.post<UserResponse>(url, user).pipe(
+    map((response) => response.response),
+    map((userResponse) => ({ ...userResponse, userID: userResponse.id }))
+  );
 }
