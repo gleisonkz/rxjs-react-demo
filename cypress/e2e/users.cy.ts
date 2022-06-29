4682e09cccf34b61e86e92ed6d86330b274bdf7f
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-06-29 07:13:26 -0300

    :test_tube: test: fix broken tests

--- cypress/e2e/users.cy.ts
+++ cypress/e2e/users.cy.ts
@@ -1,37 +1,30 @@
 describe("[RxJS-React-Demo] - User Flow", () => {
+  const FIRST_USER = {
+    id: 1,
+    name: "George",
+    avatar: "https://reqres.in/img/faces/1-image.jpg",
+    age: 23,
+  };
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-    cy.intercept("GET", "https://reqres.in/api/users", {
+    cy.intercept("GET", "http://localhost:3200/users", {
       statusCode: 200,
-      body: {
-        data: [
-          {
-            id: 1,
-            email: "george.bluth@reqres.in",
-            first_name: "George",
-            last_name: "Bluth",
-            avatar: "https://reqres.in/img/faces/1-image.jpg",
-            age: 23,
-          },
+      body: [
+        FIRST_USER,
           {
             id: 2,
-            email: "janet.weaver@reqres.in",
-            first_name: "Janet",
-            last_name: "Weaver",
+          name: "Janet",
             avatar: "https://reqres.in/img/faces/2-image.jpg",
             age: 35,
           },
           {
             id: 3,
-            email: "emma.wong@reqres.in",
-            first_name: "Emma",
-            last_name: "Wong",
+          name: "Emma",
             avatar: "https://reqres.in/img/faces/3-image.jpg",
             age: 25,
           },
         ],
-      },
-    }).as("getUsers");
+    }).as("fake getUsers");
   });
 
   it("should render page with no selected user by default", () => {
@@ -45,10 +38,10 @@ describe("[RxJS-React-Demo] - User Flow", () => {
 
   it("should assign user to selected user when click on select button", () => {
     cy.getByTestId("select-user-button").first().click();
-    cy.getByTestId("selected-user").should("contain", "George Bluth");
+    cy.getByTestId("selected-user").should("contain", FIRST_USER.name);
   });
 
-  it("should set selected user and icrement age when click on increment button", () => {
+  it("should set selected user and increment age when click on increment button", () => {
     cy.getByTestId("select-user-button").first().click();
     cy.getByTestId("increment-age-button").first().click();
     cy.getByTestId("age").should("contain", "24");
